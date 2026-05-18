0a3400d3603e304139cc65b86ddc2b82789422c1
NicoCasadidio <nicocasadidio05@gmail.com>
2026-05-18 16:57:55 -0300

    add clerk webhook to sync users with database

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ['arson-graveyard-manger.ngrok-free.dev'],
 };
 
 export default nextConfig;
